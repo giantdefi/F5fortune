1840a2af2ceaedb455fcb53a57b83cf85842da1b
unknown <cryptonativecoding@gmail.com>
2024-09-01 12:10:21 +0700

    commit 7 login process

--- src/components/inputforms/login/Email.js
+++ src/components/inputforms/login/Email.js
@@ -31,16 +31,16 @@ export default function EmailForgot() {
     return (
         <>
           
-            <div class="max-w-sm mx-auto">
-  <label for="website-admin" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Email : </label>
-  <div class="flex">
-    <span class="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border border-e-0 border-gray-300 rounded-s-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
-    <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 16">
+            <div className="max-w-sm mx-auto">
+  <label  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Email : </label>
+  <div className="flex">
+    <span className="inline-flex items-center px-3 text-sm text-gray-900 bg-gray-200 border border-e-0 border-gray-300 rounded-s-md dark:bg-gray-600 dark:text-gray-400 dark:border-gray-600">
+    <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 16">
         <path d="m10.036 8.278 9.258-7.79A1.979 1.979 0 0 0 18 0H2A1.987 1.987 0 0 0 .641.541l9.395 7.737Z"/>
         <path d="M11.241 9.817c-.36.275-.801.425-1.255.427-.428 0-.845-.138-1.187-.395L0 2.6V14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2.5l-8.759 7.317Z"/>
       </svg>
     </span>
-    <input type="email"  class="rounded-none rounded-e-lg bg-gray-50 border border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white 
+    <input type="email"  className="rounded-none rounded-e-lg bg-gray-50 border border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 block flex-1 min-w-0 w-full text-sm p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white 
     dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="jhondoe@gmail.com"
     name="email"
     value = {email || ""}
