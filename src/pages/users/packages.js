147debaec80c11e004f89c1f8d50e2c1dbfe55c1
unknown <cryptonativecoding@gmail.com>
2024-09-02 16:58:59 +0700

    commit 10 oackage title desc

--- src/pages/users/packages.js
+++ src/pages/users/packages.js
@@ -67,7 +67,7 @@ export default function Users() {
   <div className="py-8 px-4 mx-auto  lg:py-16 lg:px-6">
       <div className="mx-auto max-w-screen-lg text-center mb-8 lg:mb-12">
           <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Designed for business teams like yours</h2>
-          <p className="mb-5 font-light text-gray-500 sm:text-xl dark:text-gray-400">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
+          <p className="mb-5 font-light text-gray-500 sm:text-xl dark:text-gray-400">Here at Fivefortune we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
       </div>
       <div className="space-y-8 lg:grid lg:grid-cols-2 xl:grid-cols-3 sm:gap-6 xl:gap-10 lg:space-y-0 w-full xl:w-2/3 mx-auto">
           
